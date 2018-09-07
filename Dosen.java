8b4e4227573376b065469a8d61c4b8f04c89a53b
selinamrzk <selinamaurizka@gmail.com>
2018-09-07 15:18:34 +0700

    Adding Dosen.java

---
+++ Dosen.java
@@ -0,0 +1,23 @@
+
+public class Dosen {
+	String nama;
+	String nik;
+	
+	public Dosen(String nama, String nik) {
+		super();
+		this.nama = nama;
+		this.nik = nik;
+	}
+	public String getNama() {
+		return nama;
+	}
+	public void setNama(String nama) {
+		this.nama = nama;
+	}
+	public String getNik() {
+		return nik;
+	}
+	public void setNpm(String nik) {
+		this.nik = nik;
+	}
+}
